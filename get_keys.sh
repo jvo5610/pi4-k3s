931ae8febf8998d4a71abca5973504f027d69d1f
jvo5610 <jvidelaolmos@gmail.com>
2020-12-09 23:27:49 -0300

    script fixes

--- get_keys.sh
+++ get_keys.sh
@@ -3,7 +3,7 @@
 # 2nd argument = zerotier_network_id
 # 3rd argument = zerotier_token
 apt install -y curl jq
-echo -e "interface wlan0\nstatic ip_address=$1\nstatic routers=192.168.1.1\nstatic domain_name_servers=186.183.22.1" >> /etc/_dhcpcd.conf
+echo -e "interface wlan0\nstatic ip_address=$1\nstatic routers=192.168.1.1\nstatic domain_name_servers=186.183.22.1" >> /etc/dhcpcd.conf
 sudo -u pi ssh-keygen -t rsa -N ''
 for row in $(curl 'https://api.github.com/users/jvo5610/keys' | jq '.[] | .key')
     do
@@ -11,6 +11,6 @@ for row in $(curl 'https://api.github.com/users/jvo5610/keys' | jq '.[] | .key')
     done
 sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
 curl -s https://install.zerotier.com | sudo bash
-sudo zerotier-cli join $1
+sudo zerotier-cli join $2
 sleep 5
 curl -X POST "https://my.zerotier.com/api/network/$2/member/$(zerotier-cli info | cut -d " " -f 3)" -H "Content-Type: application/json" -H "Authorization: bearer $3" -d "{\"config\": {\"authorized\": true} }"
