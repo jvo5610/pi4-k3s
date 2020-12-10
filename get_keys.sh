2c8d7c0fdfe6855e9c0e54afede6296c5c9b44d2
jvo5610 <jvidelaolmos@gmail.com>
2020-12-10 01:55:47 -0300

    final commit

--- get_keys.sh
+++ get_keys.sh
@@ -2,10 +2,11 @@
 # 1st argument = IP -> 192.168.1.150 = master , then  192.168.1.151, ... workers
 # 2nd argument = zerotier_network_id
 # 3rd argument = zerotier_token
+# 4rd argument = github username
 apt install -y curl jq
 echo -e "interface wlan0\nstatic ip_address=$1\nstatic routers=192.168.1.1\nstatic domain_name_servers=186.183.22.1" >> /etc/dhcpcd.conf
 sudo -u pi ssh-keygen -t rsa -N ''
-for row in $(curl 'https://api.github.com/users/jvo5610/keys' | jq '.[] | .key')
+for row in $(curl "https://api.github.com/users/$4/keys" | jq '.[] | .key')
     do
     echo $row | tr -d '"' | tr -d "\n" | sed 's/ssh-rsa/\nssh-rsa /g' >> /home/pi/.ssh/authorized_keys
     done
--- set_static_ip.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-# IP arguments 192.168.1.150 = master , 192.168.1.151, ... workers
-echo -e "interface wlan0\nstatic ip_address=$1\nstatic routers=192.168.1.1\nstatic domain_name_servers=186.183.22.1" >> /etc/_dhcpcd.conf
